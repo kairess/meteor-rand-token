2e33b9cf7f859e4afb2a0af4fb22d697bf6dfdde
Brad <th.lee@ecubelabs.com>
2015-06-18 10:35:23 +0900

    change version

--- package.js
+++ package.js
@@ -1,10 +1,10 @@
 Package.describe({
   name: 'taehee:rand-token',
-  version: '0.0.2',
+  version: '0.0.3',
   // Brief, one-line summary of the package.
   summary: 'Generate random tokens from your choice of randomness.',
   // URL to the Git repository containing the source code for this package.
-  git: '',
+  git: 'https://github.com/kairess/meteor-rand-token.git',
   // By default, Meteor will default to using README.md for documentation.
   // To avoid submitting documentation, set this field to null.
   documentation: 'README.md'
